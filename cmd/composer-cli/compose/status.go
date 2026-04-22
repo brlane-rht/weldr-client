23e76d5d8bfe44a805c86ac51ca14093427ff3b2
Brian C. Lane <bcl@redhat.com>
2026-04-22 15:00:59 -0700

    composer-cli: Use new FilterCompose helper for status command
    This filters weldr api compose uuids from the list of cloud api
    composes. These duplicates happen when a compose is started using the
    weldrapi. It also depends on a pending osbuild-composer PR:
    https://github.com/osbuild/osbuild-composer/pull/5110
    
    Resolves: HMS-10332

--- cmd/composer-cli/compose/status.go
+++ cmd/composer-cli/compose/status.go
@@ -12,8 +12,8 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"github.com/osbuild/weldr-client/v2/cloud"
 	"github.com/osbuild/weldr-client/v2/cmd/composer-cli/root"
-	"github.com/osbuild/weldr-client/v2/weldr"
 )
 
 var (
@@ -35,25 +35,36 @@ func status(cmd *cobra.Command, args []string) (rcErr error) {
 	w := tabwriter.NewWriter(os.Stdout, 5, 0, 3, ' ', 0)
 	fmt.Fprintln(w, "ID\tStatus\tTime\tBlueprint\tVersion\tType\tSize")
 
+	// Get the weldrapi composes first
+	// Response to any errors happens after checking the cloudapi
+	// This is so that any weldrapi composes can be filtered out of the cloudapi
+	// response.
+	weldrComposes, errors, err := root.Client.ListComposes()
+	var weldrUUIDs []string
+	for _, wc := range weldrComposes {
+		weldrUUIDs = append(weldrUUIDs, wc.ID)
+	}
+
 	// Check cloudapi for composes first
 	if root.Cloud.Exists() {
 		composes, _ := root.Cloud.ListComposes()
+		// Filter out any weldrapi composes
+		composes = cloud.FilterComposes(composes, nil, weldrUUIDs)
 		if len(composes) > 0 {
-			for i := range composes {
+			for _, compose := range composes {
 				// Get as much detail as we can about the compose
 				// This depends on the type of build and how it was started so some fields may
 				// be blank. Currently no details are available so they are left blank.
-				bpName, bpVersion, imageType, size := composeDetails(composes[i].ID)
-				fmt.Fprintf(w, "%s\t%-8s\t%s\t%-15s\t%s\t%-16s\t%s\n", composes[i].ID,
-					root.Cloud.StatusMap(composes[i].Status),
+				bpName, bpVersion, imageType, size := composeDetails(compose.ID)
+				fmt.Fprintf(w, "%s\t%-8s\t%s\t%-15s\t%s\t%-16s\t%s\n", compose.ID,
+					root.Cloud.StatusMap(compose.Status),
 					"",
 					bpName, bpVersion, imageType, size)
 			}
 		}
 	}
 
-	// Check weldrapi for composes
-	composes, errors, err := root.Client.ListComposes()
+	// Handle weldr errors after printing cloudapi composes
 	if err != nil {
 		return root.ExecutionError(cmd, "List Error: %s", err)
 	}
@@ -61,10 +72,7 @@ func status(cmd *cobra.Command, args []string) (rcErr error) {
 		rcErr = root.ExecutionErrors(cmd, errors)
 	}
 
-	composes = weldr.SortComposeStatusV0(composes)
-	for i := range composes {
-		c := composes[i]
-
+	for _, c := range weldrComposes {
 		// Convert the API's float64 time to Time
 		var s float64
 		if c.JobFinished > 0 {
